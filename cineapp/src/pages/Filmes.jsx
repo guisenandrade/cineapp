cea6d16532c0b025327ab77536ec4c528dbb5285
GUILHERME SENA ANDRADE <RM13637@fiap.com.br>
2024-05-29 10:05:43 -0300

    final

--- cineapp/src/pages/Filmes.jsx
+++ cineapp/src/pages/Filmes.jsx
@@ -4,16 +4,7 @@ function Filmes() {
     return ( 
         <>
           <h1>Filmes</h1>
-        {
-            data.map(
-                filme =>(
-                    <>
-                    <h1 key={filme.title}>{filme.title}</h1>
-                    <img src={filme.image}/>
-                    </>
-                )
-            )
-        }
+       
         </>
       
      );
