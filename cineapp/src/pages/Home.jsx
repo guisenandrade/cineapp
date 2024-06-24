9cc865fbf11778b7bf682d4f50824d433f67d08e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-24 13:39:20 -0300

    final

--- cineapp/src/pages/Home.jsx
+++ cineapp/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react'
 import{Link} from 'react-router-dom'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Home(){
-
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
     const[filmes,setFilmes] = useState([])
 
     useEffect( () =>{
@@ -16,29 +25,78 @@ function Home(){
             <>
             <img className=' w-[1100px] h-[450px] ml-28 mb-10'  src="../../public/mortais.jpg" />
             <h1 className='font-normal text-4xl mb-7 ml-28'> NOVOS NO CIMENA</h1>
-            <div className="grid grid-cols-3">
-                {
-                   filmes.map(
+            <div className='flex justify-center'>
+        <div className='w-[1150px]'>
+          <Slider {...settings}>
+            {
+                filmes.map(
                     (filme) =>(
                         <div className="card" key={filme.id}>
-                           
-                            <img className='rounded-xl w-52 h-72 ml-28' src={`https://image.tmdb.org/t/p/w200/${filme.poster_path}`} alt={filme.title}/>
-                            
-                            <div className='bg-terciary-sena w-52 h-auto rounded-xl ml-28 mb-8'>
-                            <Link to={`${filme.id}`}>
-                            <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>
-                            <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
-                            </Link>
+                       
+                        <img className='rounded-xl w-52 h-72 ml-28' src={`https://image.tmdb.org/t/p/w200/${filme.poster_path}`} alt={filme.title}/>
+                        
+                        <div className='bg-terciary-sena w-52 h-auto rounded-xl ml-28 mb-8'>
+                        <Link to={`${filme.id}`}>
+                          <div className='flex-row'>
+                          <div className='flex flex-col'>
+                          <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>   
+                          <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
+                          </div>
+
                             </div>
                           
-                            
+
+                        
+                        </Link>
                         </div>
-                    )
+                      
+                        
+                    </div>
                 )
-                }
-           
-            </div>
-            </>
+                )
+              }
+          </Slider>
+
+          <div className='flex justify-center'>
+          <div className='w-[1150px]'>
+          <h1 className='font-normal text-4xl mb-7 ml-28'> MAIS POPULARES</h1>
+
+          <Slider {...settings}>
+            {
+                filmes.map(
+                    (filme) =>(
+                        <div className="card" key={filme.id}>
+                       
+                        <img className='rounded-xl w-52 h-72 ml-28' src={`https://image.tmdb.org/t/p/w200/${filme.poster_path}`} alt={filme.title}/>
+                        
+                        <div className='bg-terciary-sena w-52 h-auto rounded-xl ml-28 mb-8'>
+                        <Link to={`${filme.id}`}>
+                          <div className='flex-row'>
+                          <div className='flex flex-col'>
+                          <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>   
+                          <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
+                          </div>
+
+                            </div>
+                          
+
+                        
+                        </Link>
+                        </div>
+                      
+                        
+                    </div>
+                )
+                )
+              }
+          </Slider>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div className='bg-secondary-sena h-[300px]'></div>
+        </>
+        
         )
     }
      export default Home
--- cineapp/src/pages/Sobre.jsx
+++ cineapp/src/pages/Sobre.jsx
@@ -1,7 +1,18 @@
 import { useState, useEffect } from 'react'
 import{Link} from 'react-router-dom'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Sobre() {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
 
     const[filmes,setFilmes] = useState([])
 
@@ -14,20 +25,31 @@ function Sobre() {
 
     return (
         <>
+        
           <h1 className='font-normal text-4xl mb-7 ml-28 text-red-700'>FAVORITOS</h1>
           <img className='w-14 h-14 absolute left-[285px] top-[90px]' src="../../public/coracao.png" alt="" />
-          <div className="grid grid-cols-3">
+          
+          <div className='flex justify-center'>
+          <div className='w-[1150px]'>
+            <Slider {...settings}>
               {
-                 filmes.map(
-                  (filme) =>(
-                      <div className="card" key={filme.id}>
+                  filmes.map(
+                      (filme) =>(
+                          <div className="card" key={filme.id}>
                          
                           <img className='rounded-xl w-52 h-72 ml-28' src={`https://image.tmdb.org/t/p/w200/${filme.poster_path}`} alt={filme.title}/>
                           
                           <div className='bg-terciary-sena w-52 h-auto rounded-xl ml-28 mb-8'>
                           <Link to={`${filme.id}`}>
-                          <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>
-                          <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
+                            <div className='flex-row'>
+                            <div className='flex flex-col'>
+                            <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>   
+                            <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
+                            </div>
+                            <img className="w-10 p-1 ml-[160px]" src="../../public/coracao.png" alt="" />
+                            </div>
+                            
+
                           
                           </Link>
                           </div>
@@ -35,11 +57,49 @@ function Sobre() {
                           
                       </div>
                   )
-              )
-              }
-         
+                  )
+                }
+            </Slider>
+
+            <div className='flex justify-center'>
+            <div className='w-[1150px]'>
+
+            <Slider {...settings}>
+              {
+                  filmes.map(
+                      (filme) =>(
+                          <div className="card" key={filme.id}>
+                         
+                          <img className='rounded-xl w-52 h-72 ml-28' src={`https://image.tmdb.org/t/p/w200/${filme.poster_path}`} alt={filme.title}/>
+                          
+                          <div className='bg-terciary-sena w-52 h-auto rounded-xl ml-28 mb-8'>
+                          <Link to={`${filme.id}`}>
+                            <div className='flex-row'>
+                            <div className='flex flex-col'>
+                            <h1 className='font-normal text-lg ml-4' key={filme.title}>{filme.title}</h1>   
+                            <span className='font-normal text-quarter-sena ml-4' key={filme}>Nota do Filme : {filme.vote_average.toFixed()}</span>
+                            </div>
+                            <img className="w-10 p-1 ml-[160px]" src="../../public/coracao.png" alt="" />
+                            </div>
+                            
+
+                          
+                          </Link>
+                          </div>
+                        
+                          
+                      </div>
+                  )
+                  )
+                }
+            </Slider>
+            </div>
+            </div>
           </div>
+          </div>
+          <div className='bg-secondary-sena h-[300px]'></div>
           </>
+          
     )
 }
 
